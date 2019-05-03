a0468386ba9dc3d5b3a467c04cece156a4cbae98
Athena Liu <sdathena@mail.ubc.ca>
2019-05-03 10:28:01 -0700

    Add mutation to create link

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -22,3 +22,27 @@ class Query(graphene.ObjectType):
         if id is not None:
             return Link.objects.get(pk=id)
         return None
+
+
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
